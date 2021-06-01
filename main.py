441558f128224f1348101012d3b53fa0019860f2
Muhammad Audrian Ananda Priambodo <rianfiftyseven@gmail.com>
2021-06-01 16:32:18 +0700

    Added better tempfile and tempdir handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import urllib.request
+import tempfile
 from pathlib import Path
 
 from flask import *
@@ -7,15 +8,12 @@ from werkzeug.utils import secure_filename
 
 from predict import Predictor
 
-UPLOAD_FOLDER = '/tmp/heroscan'
+UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 # 8 MB
-
- # Create temp upload dir
-Path(UPLOAD_FOLDER).mkdir(parents=True, exist_ok=True)
 
 def allowed_file(filename):
     return '.' in filename and filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS 
@@ -27,7 +25,6 @@ def hello():
 @app.route('/api/predict/', methods=['POST'])
 def predict():
     predictor = Predictor()
-    response = {}
     '''Predicts which hero is in the uploaded image.'''
     if request.method == 'POST':
         # Verify file is in POST request
@@ -35,18 +32,21 @@ def predict():
             response = jsonify({'message': 'Request contains no \'file\' part.'})
             response.status_code = 400
             return response
+
         image = request.files['file']
+        
         if image and allowed_file(image.filename):
-            # Make temp file for prediction
-            filename = secure_filename(image.filename)
-            temp_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            image.save(temp_file)
+            # Create temporary dir
+            with tempfile.TemporaryDirectory(dir=UPLOAD_FOLDER) as temp_dir:
+                # Make temp file for prediction
+                with tempfile.TemporaryFile(dir=temp_dir) as temp_file:
+                    image.save(temp_file)
 
-            # Prediction logic
-            result = predictor.predict(image)
+                    # Prediction logic
+                    result = predictor.predict(temp_file)
 
-            # Cleanup
-            os.remove(temp_file)
+                # Cleanup
+                # temp_file.close()
 
             # Return result as JSON response
             return jsonify(result)
