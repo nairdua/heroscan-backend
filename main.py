b12c4bcc271f040b0becfea7150dd9b0119f7505
Muhammad Audrian Ananda Priambodo <rianfiftyseven@gmail.com>
2021-06-01 15:49:42 +0700

    Added Prediction REST endpoint

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
-from flask import *
-from predict import *
-app = Flask(__name__)
+import os
+import urllib.request
+from pathlib import Path
 
-@app.route('/predict', methods=['POST'])
+from flask import *
+from werkzeug.utils import secure_filename
+
+from predict import Predictor
+
+UPLOAD_FOLDER = '/tmp/heroscan'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 # 8 MB
+
+ # Create temp upload dir
+Path(UPLOAD_FOLDER).mkdir(parents=True, exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS 
+
+@app.route('/')
+def hello():
+    return "Hello, world!"
+
+@app.route('/api/predict/', methods=['POST'])
 def predict():
-    return 'Prediction URL'
+    predictor = Predictor()
+    response = {}
+    '''Predicts which hero is in the uploaded image.'''
+    if request.method == 'POST':
+        # Verify file is in POST request
+        if 'file' not in request.files:
+            response = jsonify({'message': 'Request contains no \'file\' part.'})
+            response.status_code = 400
+            return response
+        image = request.files['file']
+        if image and allowed_file(image.filename):
+            # Make temp file for prediction
+            filename = secure_filename(image.filename)
+            temp_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image.save(temp_file)
+
+            # Prediction logic
+            result = predictor.predict(image)
+
+            # Cleanup
+            os.remove(temp_file)
+
+            # Return result as JSON response
+            return jsonify(result)
--- predict.py
+++ predict.py
@@ -10,58 +10,59 @@ SK_MODEL_PATH = 'modelheroscan.pkl'
 TF_MODEL_PATH = 'saved_model/my_model/'
 LABEL_PATH = 'labelheroscan.pkl'
 
-def extract_face(filename, required_size=(160, 160)):
-    '''Extract a single face from input file'''
-    image = Image.open(filename)
-    image = image.convert('RGB')
-    pixels = asarray(image)
+class Predictor:
+    def extract_face(self, filename, required_size=(160, 160)):
+        '''Extract a single face from input file'''
+        image = Image.open(filename)
+        image = image.convert('RGB')
+        pixels = asarray(image)
 
-    # detect faces
-    detector = MTCNN()
-    results = detector.detect_faces(pixels)
+        # detect faces
+        detector = MTCNN()
+        results = detector.detect_faces(pixels)
 
-    # extract bounding box from 1st face
-    x1, y1, width, height = results[0]['box']
+        # extract bounding box from 1st face
+        x1, y1, width, height = results[0]['box']
 
-    # bugfix
-    x1, y1 = abs(x1), abs(y1)
-    x2, y2 = x1 + width, y1 + height
+        # bugfix
+        x1, y1 = abs(x1), abs(y1)
+        x2, y2 = x1 + width, y1 + height
 
-    # extract face
-    face = pixels[y1:y2, x1:x2]
+        # extract face
+        face = pixels[y1:y2, x1:x2]
 
-    # resize pixels to model size
-    image = Image.fromarray(face)
-    image = image.resize(required_size)
-    face_array = asarray(image)
-    return face_array
+        # resize pixels to model size
+        image = Image.fromarray(face)
+        image = image.resize(required_size)
+        face_array = asarray(image)
+        return face_array
 
-def get_embedding(model, face_pixels):
-    '''Get embed for face'''
-    face_pixels = face_pixels.astype('float32')
-    mean, std = face_pixels.mean(), face_pixels.std()
-    face_pixels = (face_pixels - mean) / std
-    samples = expand_dims (face_pixels, axis=0)
-    yhat = model.predict(samples)
-    return yhat[0]
+    def get_embedding(self, model, face_pixels):
+        '''Get embed for face'''
+        face_pixels = face_pixels.astype('float32')
+        mean, std = face_pixels.mean(), face_pixels.std()
+        face_pixels = (face_pixels - mean) / std
+        samples = expand_dims(face_pixels, axis=0)
+        yhat = model.predict(samples)
+        return yhat[0]
 
-def predict(image):
-    '''get prediction from input image'''
-    image = extract_face(image)
-    tf_model = tf.keras.models.load_model(TF_MODEL_PATH)
+    def predict(self, image):
+        '''get prediction from input image'''
+        image = self.extract_face(image)
+        tf_model = tf.keras.models.load_model(TF_MODEL_PATH)
 
-    # put image into tensorflow model
-    face_emb = get_embedding(tf_model, image)
-    samples = expand_dims(face_emb, axis=0)
+        # put image into tensorflow model
+        face_emb = self.get_embedding(tf_model, image)
+        samples = expand_dims(face_emb, axis=0)
 
-    sk_model = pickle.load(open(SK_MODEL_PATH, 'rb'))
-    label = pickle.load(open(LABEL_PATH, 'rb'))
+        sk_model = pickle.load(open(SK_MODEL_PATH, 'rb'))
+        label = pickle.load(open(LABEL_PATH, 'rb'))
 
-    # pipe TF result to SK model
-    yhat_class = sk_model.predict(samples)
+        # pipe TF result to SK model
+        yhat_class = sk_model.predict(samples)
 
-    # results
-    class_index = yhat_class[0]
-    predict_names = label.inverse_transform(yhat_class)
-    result = predict_names[0]
-    return result
+        # results
+        class_index = yhat_class[0]
+        predict_names = label.inverse_transform(yhat_class)
+        result = predict_names[0]
+        return result
